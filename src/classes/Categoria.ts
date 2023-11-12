20ecb3ffb2a82c99a76ee46d9ec46a8b63239e15
João Pedro <joao_pedro01@terra.com.br>
2023-11-12 18:29:29 -0300

    Add classe Categoria

---
+++ src/classes/Categoria.ts
@@ -0,0 +1,61 @@
+class Categoria {
+    private id: number;
+    private nome: string;
+    private descricao: string;
+    private isActive: boolean;
+    private createdAt: Date;
+    private updatedAt: Date;
+
+    constructor() {
+        this.id = 0;
+        this.nome = "";
+        this.descricao = "";
+        this.isActive = false;
+        this.createdAt = new Date();
+        this.updatedAt = new Date();
+    }
+
+    public getId(): number {
+        return this.id;
+    }
+    public setId(id: number): void {
+        this.id = id;
+    }
+
+    public getNome(): string {
+        return this.nome;
+    }
+    public setNome(nome: string): void {
+        this.nome = nome;
+    }
+
+    public getDescricao(): string {
+        return this.descricao;
+    }
+    public setDescricao(descricao: string): void {
+        this.descricao = descricao;
+    }
+
+    public getIsActive(): boolean {
+        return this.isActive;
+    }
+    public setIsActive(isActive: boolean): void {
+        this.isActive = isActive;
+    }
+
+    public getCreatedAt(): Date {
+        return this.createdAt;
+    }
+    public setCreatedAt(createdAt: Date): void {
+        this.createdAt = createdAt;
+    }
+
+    public getUpdatedAt(): Date {
+        return this.updatedAt;
+    }
+    public setUpdatedAt(updatedAt: Date): void {
+        this.updatedAt = updatedAt;
+    }
+}
+
+export default Categoria;
